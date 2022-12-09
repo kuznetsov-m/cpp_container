8f113418484dcc64f2ccd136dd7bb0ca61d300d0
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2022-12-09 23:56:05 +0600

    test_app: added

---
+++ test_app/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+#include <vector.h>
+#include "../test/test_object.h"
+
+int main() {
+    // std::cout << "this is test app" << std::endl;
+    return 0;
+}
